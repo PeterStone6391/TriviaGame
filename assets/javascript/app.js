8b54fdafb9ef6f0264bc6d0b9d3d164ea472e02e
Peter Stone <peter.stone6391@gmail.com>
2019-03-09 17:22:58 -0600

    added css and html

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Come up with 10 questions------ Choose Disney
+// come up with the possible answers for each question-----  Found some questions on a Buzzfeed quiz that has three wrong and one right answer https://www.buzzfeed.com/briangalindo/the-ultimate-disney-trivia-quiz
+// function to start game
+// timer for page for question
+// on click event when they select answer
+// check user input vs correct answer to question (results at the end of the game)
+// once answered call new page with new question
+// repeat these steps until final question in the array
+// once array is complete launch results page
+// alert or some action for victory maybe 7/10 or better and the opposite for defeat
+// function to restart the game to play again------still need help on this one
+
+//create array of objects(one question and 4 possible answers and one correct answers)
+
+// doing the single page instead, seemed easier------
+// write the jQuery next
+// practice write functions 50 times this weekend per tutor Michael------done!
