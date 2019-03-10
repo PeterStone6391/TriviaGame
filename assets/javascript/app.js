210ba965a11a239288480f404445d2d8e4432e6f
Peter Stone <peter.stone6391@gmail.com>
2019-03-09 20:36:47 -0600

    finished creating my questions and answer object arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,23 +24,30 @@ var questions = [
       "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
     ],
     name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
+    correct: "Magic mirror, on the wall — who is the fairest one of all?",
     divClass: ".snowWhiteQueen"
   },
   {
     ques:
-      "Finish the lyrics: “Wouldn't you think I'm the girl, the girl who has everything? Look at this trove, treasures untold…”,
+      "Finish the lyrics: “Wouldn't you think I'm the girl, the girl who has everything? Look at this trove, treasures untold…",
     ans: [
-      "It’s full of gizmos and gadgets galore.", "Wonders from all over the world.", "There so much to be known.", "How many wonders can one cavern hold?"
+      "It’s full of gizmos and gadgets galore.",
+      "Wonders from all over the world.",
+      "There so much to be known.",
+      "How many wonders can one cavern hold?"
     ],
     name: "littleMermaid",
     correct: "How many wonders can one cavern hold?",
     divClass: ".littleMermaid"
   },
   {
-    ques: "What is the name of the organization Bernard and Miss Bianca work in The Rescuers?",
+    ques:
+      "What is the name of the organization Bernard and Miss Bianca work in The Rescuers?",
     ans: [
-      "The Rescue Aid Society", "The International Rescue Organization", "The Rescuers", "The Secret Helpers"
+      "The Rescue Aid Society",
+      "The International Rescue Organization",
+      "The Rescuers",
+      "The Secret Helpers"
     ],
     name: "theRescuers",
     correct: "The Rescue Aid Society",
@@ -49,70 +56,69 @@ var questions = [
   {
     ques: "Why is the Horned King searching for the Black Cauldron?",
     ans: [
-      "It has the power to create an army of undead warriors.", "It has the power to bring him back to life." , "It has the power to control the minds of humans.", "It has the power to help him destroy the world."
+      "It has the power to create an army of undead warriors.",
+      "It has the power to bring him back to life.",
+      "It has the power to control the minds of humans.",
+      "It has the power to help him destroy the world."
     ],
     name: "theBlackCauldron",
     correct: "It has the power to create an army of undead warriors.",
     divClass: ".theBlackCauldron"
   },
   {
-    ques: "Which phrase does the Evil Queen in Snow White actually say?",
+    ques:
+      "In Pocahontas, what did Pocahontas see in her dream that made her believe that a change was coming?",
     ans: [
-      "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
+      "A spinning arrow",
+      "A strange cloud formation",
+      "A hawk circling her village",
+      "A burning blue fire"
     ],
-    name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
-    divClass: ".snowWhiteQueen"
+    name: "pocahontas",
+    correct: "A spinning arrow",
+    divClass: ".pocahontas"
   },
   {
-    ques: "Which phrase does the Evil Queen in Snow White actually say?",
-    ans: [
-      "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
-    ],
-    name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
-    divClass: ".snowWhiteQueen"
+    ques:
+      "In Hercules, Hades promised not to harm Megara if Hercules gave up his strength for how long?",
+    ans: ["12 hours", "24 hours", "36 hours", "48 hours"],
+    name: "hercules",
+    correct: "24",
+    divClass: ".hercules"
   },
   {
-    ques: "Which phrase does the Evil Queen in Snow White actually say?",
-    ans: [
-      "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
-    ],
-    name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
-    divClass: ".snowWhiteQueen"
+    ques:
+      "On whose shoulders does Dopey stand on in order to dance with Snow White during “The Silly Song” scene?",
+    ans: ["Doc", "Grumpy", "Sneezy", "Happy"],
+    name: "snowWhiteDwarfs",
+    correct: "Sneezy",
+    divClass: ".snowWhiteDwarfs"
   },
   {
-    ques: "Which phrase does the Evil Queen in Snow White actually say?",
-    ans: [
-      "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
-    ],
-    name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
-    divClass: ".snowWhiteQueen"
+    ques:
+      "In Alice in Wonderland, aside from a pocket watch, what else is the White Rabbit holding when Alice encounters him?",
+    ans: ["A cane", "An umbrella", "A top hat", "All of the above"],
+    name: "aliceInWonderland",
+    correct: "An umbrella",
+    divClass: ".aliceInWonderland"
   },
   {
-    ques: "Which phrase does the Evil Queen in Snow White actually say?",
-    ans: [
-      "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
-    ],
-    name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
-    divClass: ".snowWhiteQueen"
+    ques: "How many years does the Genie say he has been trapped in the lamp?",
+    ans: ["100 years", "500 years", "1,000 years", "10,000 years"],
+    name: "genie",
+    correct: "10,000 years",
+    divClass: ".genie"
   },
   {
-    ques: "Which phrase does the Evil Queen in Snow White actually say?",
+    ques: "What does the matchmaker criticize Mulan for?",
     ans: [
-      "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
+      "Being too tall.",
+      "Being too skinny.",
+      "Having bad posture.",
+      "Having big feet."
     ],
-    name: "snowWhiteQueen",
-    correct: "Magic mirror, on the wall — who is the fairest one of all?”,
-    divClass: ".snowWhiteQueen"
+    name: "mulan",
+    correct: "Being too skinny.",
+    divClass: ".mulan"
   }
 ];
