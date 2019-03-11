2e2c45e97bc6d08affb27a100ff9f278b79ada06
Peter Stone <peter.stone6391@gmail.com>
2019-03-10 22:48:23 -0500

    finsihed java but still need to do reset and button spacing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,9 @@ var questions = [
     ques: "Which phrase does the Evil Queen in Snow White actually say?",
     ans: [
       "Mirror, mirror, on the wall — who is the fairest of them all?",
-      "Magic mirror, on the wall — who is the fairest one of all?”, “Mirror, mirror, on the wall — who is the fairest one of all?”, “Magic mirror, on the wall — who is the fairest of them all?”"
+      "Magic mirror, on the wall — who is the fairest one of all?",
+      "Mirror, mirror, on the wall — who is the fairest one of all?",
+      "Magic mirror, on the wall — who is the fairest of them all?"
     ],
     name: "snowWhiteQueen",
     correct: "Magic mirror, on the wall — who is the fairest one of all?",
@@ -122,3 +124,120 @@ var questions = [
     divClass: ".mulan"
   }
 ];
+var labels = ["first", "second", "third", "forth"];
+
+// click to start then display questions
+var startGame = $("#start-btn").on("click", function() {
+  $(this)
+    .parent()
+    .hide();
+  $(".container").show();
+  countdown(120);
+  questionDisplay();
+});
+
+// function for displaying questions using j and i. as tom talked in class we use i,j,k, etc for standards
+var questionDisplay = function() {
+  $(".questions :not('#sub-but')").empty();
+  // loops through the 10 questions
+  for (var j = 0; j < 10; j++) {
+    $(".questions").prepend('<div class="' + questions[j].name + '"></div>');
+    $(questions[j].divClass).append(
+      '<div class ="ques-title">' + questions[j].ques + "</div>"
+    );
+    // loops through answers for each radio button
+    for (var i = 0; i <= 3; i++) {
+      $(questions[j].divClass).append(
+        '<input type="radio"  name="' +
+          questions[j].name +
+          '" value="' +
+          questions[j].ans[i] +
+          '"/><label for="' +
+          labels[i] +
+          '">' +
+          questions[j].ans[i] +
+          "</label>"
+      );
+    }
+    $(".questions").prepend("<hr />");
+  }
+};
+
+// function for countdown timer
+var countdown = function(seconds) {
+  var timer = setInterval(function() {
+    seconds = seconds - 1;
+    $("#time-remain").html(seconds);
+
+    if (seconds <= 0) {
+      $(".container").fadeOut(500);
+      var correctAnswers = 0;
+      var wrongAnswers = 0;
+      var unAnswered = 0;
+
+      // loop through correctArray & radioName to match html elements & answers
+      for (var i = 0; i < 10; i++) {
+        if (
+          $('input:radio[name="' + questions[i].name + '"]:checked').val() ===
+          questions[i].correct
+        ) {
+          correctAnswers++;
+          console.log("this is correct! number:" + i);
+        } else {
+          wrongAnswers++;
+          console.log("this is wrong! number:" + i);
+        }
+      }
+      $("#correctTimesUp").append(correctAnswers);
+      // display wrongAnswers
+      $("#wrongTimesUp").append(wrongAnswers);
+      $("#timesUp")
+        .fadeIn(1000)
+        .show();
+
+      // alert("Times Up!");
+      clearInterval(timer);
+      return;
+    }
+  }, 1000);
+
+  // click event for submit button to stop timer
+  $("#sub-but").on("click", function() {
+    clearInterval(timer);
+  });
+}; // end countdown
+
+// function to grade quiz once submit button is clicked
+var gradeQuiz = $("#sub-but").on("click", function() {
+  var correctAnswers = 0;
+  var wrongAnswers = 0;
+  var unAnswered = 0;
+
+  // loop through correctArray & radioName to match html elements & answers
+  for (var i = 0; i < 10; i++) {
+    if (
+      $('input:radio[name="' + questions[i].name + '"]:checked').val() ===
+      questions[i].correct
+    ) {
+      correctAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+  }
+
+  // once submit is clicked...
+  // tests
+  // stop timer
+  countdown();
+  // questions fade out
+  $(".container").fadeOut(500);
+  // show answerScreen
+  $("#answerScreen").show();
+  // display correctAnswers
+  $("#correctScreen").append(correctAnswers);
+  // display wrongAnswers
+  $("#wrongScreen").append(wrongAnswers);
+}); // end gradeQuiz
+
+// can't seem to figure how to get the question answers and button selections to line up
+// need to make a reset function*************
